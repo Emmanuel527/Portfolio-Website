35e4d27ed244a5b7d18e3ae95ac647439fcbd297
Emmanuel527 <arowoseluemmanuel@gmail.com>
2022-04-20 20:52:33 -0500

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header'
+
+const header = () => {
+  return (
+    <header>
+      <div className='container header_container'>
+        <h5>Hello I'm</h5>
+        <h1>Arowoselu Emmanuel</h1>
+        <h5 className='text-light'>React Developer</h5>
+      </div>
+    </header>
+  )
+}
+
+export default header
