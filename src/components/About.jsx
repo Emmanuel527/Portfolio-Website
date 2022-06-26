0fa6a39a7bb3dcd4dedd59bd75f83139764d9751
Emmanuel527 <arowoseluemmanuel@gmail.com>
2022-06-26 09:30:42 -0500

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
             <p >Nice to meet you. This is a little bit about me.</p>
           </div>
           <div>
-            <p>I love to create websites, web applications, and solve problems.</p>
+            <p>I create websites and web applications using a combination of tools such as HTML, CSS, JavaScript, React, Next.js, Gatsby.js, Node.js, MongoDB and Express.js.</p>
             <p >
             I’m focused on anything that sparks my curiosity. Other things I do besides coding include playing the guitar, making generative art and thinking about the beautiful wonders of life every once in a while.
             </p>
